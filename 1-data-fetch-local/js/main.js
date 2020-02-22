82701f33b3a74f960de3028b1befdcb42ecc2990
steviemj17 <55863655+steviemj17@users.noreply.github.com>
2020-02-21 18:00:53 -0800

    part completed

--- 1-data-fetch-local/js/main.js
+++ 1-data-fetch-local/js/main.js
@@ -1,13 +1,17 @@
-//TODO create a function that accepts data and return a list item
 
-//TODO add the URL to the fetch command to pickup the local data.json file
-fetch()
+function createPersonEl(title, firstname, lastname, email) {
+  let nameEl = document.createElement('li')
+  nameEl.innerHTML = `${title} ${firstname} ${lastname}, ${email}`
+  return nameEl
+}
+fetch('data.json')
   .then(function (response) {
-    //TODO return a JSON object 
+    return response.json();
   })
   .then(function (myJson) {
     let personList = document.querySelector('.person-list')
-    //TODO iterate through json data, create list elements and append to the html list
+    for (let person of myJson.persons) {
+      personList.appendChild(createPersonEl(person.title, person.firstname, person.lastname, person.email))
+    }
   });
 
-  

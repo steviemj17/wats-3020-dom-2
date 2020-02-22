7b8acf825306fadcdb5fb4911a61b5a9ae3e6b94
steviemj17 <55863655+steviemj17@users.noreply.github.com>
2020-02-21 18:18:55 -0800

    part 3 completed

--- 3-data-jquery-internet/js/main.js
+++ 3-data-jquery-internet/js/main.js
@@ -1,14 +1,13 @@
-function createPersonEl(title,firstname,lastname,email) {
+function createPersonEl(title, firstname, lastname, email) {
   let nameEl = $("<li>")[0]
   nameEl.append(`${title} ${firstname} ${lastname} ${email}`)
   return nameEl
 }
-//TODO call jQuery get function with the randomuse.me api and request 3 results
 
+$.get("https://randomuser.me/api/", { results: 3 })
   .done(function (response) {
     let personList = $(".person-api-list")[0]
     for (let person of response.results) {
       personList.append(createPersonEl(person.name.title, person.name.first, person.name.last, person.email))
     }
   })
-  
